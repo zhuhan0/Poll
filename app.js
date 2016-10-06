48ee85fbc3f2e89300855c91a25b3df327d355d6
Han Zhu <zhuhan7737@gmail.com>
2016-10-06 16:18:29 -0400

    added comments

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// create express server.
 var express = require('express');
 var app = express();
 
+// link ejs files as webpages.
 app.set('view engine', 'ejs');
 
+// create routes.
 var routes = require('./routes');
 
+// add static resources, which are stored in /public.
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
